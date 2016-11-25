4d40a57db40602fb0f2dcd459b2be0c473085b13
wlperry <wlperry@gmail.com>
2016-11-25 14:09:06 +0100

    do you have to commit every time to push?

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("~/Documents/website/")
+setwd("~/Documents/GitHub R Code/wlperry.github.io/")
 
 library(rmarkdown)
 
